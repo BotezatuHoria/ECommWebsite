60faa5455f14e25db3d0cea116591c117e5919d8
Botezatu Horia <horia12btz@gmail.com>
2024-07-25 18:39:13 +0300

    Added option bar

--- app/Components/opt-bar.jsx
+++ app/Components/opt-bar.jsx
@@ -1,3 +1,32 @@
+// "use client";
+// import Image from "next/image";
+
 export function OptionBar() {
-  return <h1>There will be options here</h1>;
+  return (
+    <nav
+      id="optionBar"
+      className="flex items-center justify-between bg-gray-500 px-7 py-3"
+    >
+      <div>
+        <label className="px-8 cursor-pointer hover:text-gray-300">
+          Products
+        </label>
+        <label className="px-8 cursor-pointer hover:text-gray-300">
+          Today's deals
+        </label>
+        <label className="px-8 cursor-pointer hover:text-gray-300">
+          Buy again
+        </label>
+        <label className="px-8 cursor-pointer hover:text-gray-300">
+          Latest arrivals
+        </label>
+        <label className="px-8 cursor-pointer hover:text-gray-300">Sell</label>
+      </div>
+      <div>
+        <label className="px-8 cursor-pointer hover:text-gray-300">
+          Customer service
+        </label>
+      </div>
+    </nav>
+  );
 }
